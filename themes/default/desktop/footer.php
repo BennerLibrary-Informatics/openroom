fe234913e7704dda560898663fc87682b2f5a7be
rankineugeneallen <rankineugeneallen@gmail.com>
2018-04-09 21:05:34 -0500

    Removed change to mobile view button
    Don't need it anymore. Also added some margin to the bottom to compensate

--- themes/default/desktop/footer.php
+++ themes/default/desktop/footer.php
@@ -5,10 +5,5 @@
 Developed by Ball State University Libraries<br/> <!--Put inside div block-->
 Copyright (C) 2012 Ball State University Libraries
 </div>
-<div id="themeswitch" onClick="javascript:document.cookie='theme=mobile';location.reload(true);">Switch to Mobile View <!--Eventually Remove-->
-</div>
-<br/><br/><br/> <!--Remove these later-->
-<br/><br/><br/>
-<br/><br/><br/>
 </body>
 </html>
