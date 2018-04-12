e7c4062eb4a16684dbf65a87957393dcce4b6cc0
Tyler <king.tyler.j@gmail.com>
2018-04-12 12:13:27 -0500

    Revert "Removed change to mobile view button"
    This reverts commit fe234913e7704dda560898663fc87682b2f5a7be.

--- themes/default/desktop/footer.php
+++ themes/default/desktop/footer.php
@@ -9,5 +9,10 @@
   Copyright (C) 2012 Ball State University Libraries
 </div>
 </div>
+<div id="themeswitch" onClick="javascript:document.cookie='theme=mobile';location.reload(true);">Switch to Mobile View <!--Eventually Remove-->
+</div>
+<br/><br/><br/> <!--Remove these later-->
+<br/><br/><br/>
+<br/><br/><br/>
 </body>
 </html>
