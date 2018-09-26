a0bf281ada21afa8c004f76f7b0a2430195336f2
Noah Blunier <nmblunier@gmail.com>
2018-09-25 20:43:39 -0500

    login fix?

--- modules/login.php
+++ modules/login.php
@@ -26,11 +26,11 @@
             if (xmlHttp.readyState == 4) {
                 var xmldoc = xmlHttp.responseXML;
                 var authenticated = xmldoc.getElementsByTagName('authenticated')[0].firstChild.nodeValue;
+                alert(authenticated);
                 var errormessage = xmldoc.getElementsByTagName('errormessage')[0].firstChild;
 
                 if (authenticated == "false") {
-                    alert(errormessage.nodeValue);
-                    if (errormessage.nodeValue == "No such object" || errormessage.nodeValue == "Sorry, authentication failed." || errormessage.nodeValue == "Incorrect username or password. Please try again.") errormessage.nodeValue = "Incorrect username or password.";
+                    if (errormessage.nodeValue == "No such object") errormessage.nodeValue = "Incorrect username or password.";
                     document.getElementById('errormessage').style.visibility = "visible";
                     document.getElementById('errormessage').innerHTML = ("<strong>Error: <\/strong>" + errormessage.nodeValue);
                 }
