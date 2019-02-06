7b146f0ebc51849c38bf3e7ef02d7d04d1147fa9
cmarseneau <cmarseneau@olivet.edu>
2019-02-06 15:47:41 -0600

    Added Known Issues Button To All These Pages

--- admin/report-usernames.php
+++ admin/report-usernames.php
@@ -50,7 +50,7 @@ if (!(isset($_SESSION["username"])) || $_SESSION["username"] == "") {
     <div id="heading"><span
                 class="username"><?php echo isset($_SESSION["username"]) ? $_SESSION["username"] : "&nbsp;"; ?></span>&nbsp;<?php echo ($_SESSION["isadministrator"] == "TRUE") ? "<span class=\"isadministrator\">(Admin)</span>&nbsp;" : "";
         echo ($_SESSION["isreporter"] == "TRUE") ? "<span class=\"isreporter\">(Reporter)</span>&nbsp;" : ""; ?>
-        |&nbsp;<a href="../index.php">Public View</a>&nbsp;|&nbsp;<a href="../modules/logout.php">Logout</a></div>
+        |&nbsp;<a href="../index.php">Public View</a>&nbsp;|&nbsp;<a href="https://docs.google.com/spreadsheets/d/1J_AZaNgqMfnnQwj7eCewMU9pe71DXpIBNzVXGI_Xbfk/edit?usp=sharing" target="_blank">Known Issues</a>&nbsp;|&nbsp;<a href="../modules/logout.php">Logout</a></div>
     <div id="container">
         <center>
             <?php if ($_SESSION["isadministrator"] == "TRUE" || $_SESSION["isreporter"] == "TRUE"){
